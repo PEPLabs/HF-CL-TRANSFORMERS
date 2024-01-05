2fae530e1cbf4db993ae7f4e6db8c97f0caa83d2
RoryEiffe <roryeiffe8@gmail.com>
2024-01-05 12:09:01 -0500

    Added another function to test % of positive lines in a file.

--- src/main/lab.py
+++ src/main/lab.py
@@ -1,21 +1,36 @@
 from transformers import pipeline
+from src.utilities.sentiment_utility import get_sentiment, get_lines
 
-# Here, we set up a pipeline for sentiment analysis (ex: whether the text is positive or negative)
+# Here, we set up a pipeline for sentiment analysis 
+# (ex: whether the text is positive or negative)
 classifier = pipeline("sentiment-analysis")
 
 # We pass in this text and get back a list of dictionaries
 result = classifier("I've been waiting for a HuggingFace course my whole life.")
 
-# We print the type of the first element in the list. If we had more sentences, we would see more dictionaries in the list.
+# We print the type of the first element in the list. 
+# If we had more sentences, we would see more dictionaries in the list.
 print(result[0])
 
 # TODO: Make this function return a statement that will be classified as POSITIVE
 def return_positive_statement():
-    statement = ""
+    statement = "I love you!"
     return statement
 
 # TODO: Make this function return a statement that will be classified as NEGATIVE
 def return_negative_statement():
-    statement = ""
+    statement = "I hate you!"
     return statement
 
+# TODO: Given a path to a file, this function should return a percentage 
+# of positive sentences  in the file. Each sentence will end with a period.
+# Hint: Look in the utilities folder for helper functions
+def get_percentage_positive_sentences(file_path):
+    L = get_lines(file_path)
+    results = get_sentiment(L)
+    count = 0
+    for result in results:
+        if result['label'] == 'POSITIVE':
+            count += 1
+    percentage = count / len(results)
+    return percentage
--- src/utilities/sentiment_utility.py
+++ src/utilities/sentiment_utility.py
@@ -1,5 +1,11 @@
 from transformers import pipeline
 
+# This function takes some text and returns a sentiment analysis result
 def get_sentiment(text):
     sentiment_classifier = pipeline('sentiment-analysis')
     return sentiment_classifier(text)
+
+# This function takes a file name and returns a list of lines in the file
+def get_lines(filen):
+    with open(filen, 'r') as f:
+        return f.readlines()
